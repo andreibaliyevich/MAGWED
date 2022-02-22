0d81496bdaaa0d68d42e9af748ae5ac487c171b8
Andrei Baliyevich <andreibaliyevich@gmail.com>
2022-02-22 16:26:26 +0300

    Add Generic views for accounts app

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('organizers/',
+        views.OrganizerListView.as_view(),
+        name='organizer_list'),
+    path('organizers/<slug:profile_url>/',
+        views.OrganizerDetailView.as_view(),
+        name='organizer_detail'),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Organizer
+from .serializers import OrganizerListSerializer, OrganizerDetailSerializer
 
 
-def index(request):
-    """ Accounts page """
-    context = {
-        # 'title': _('index'),
-    }
-    return render(request, 'accounts/index.html', context)
+class OrganizerListView(generics.ListAPIView):
+    """ Organizer List View """
+    queryset = Organizer.objects.all()
+    serializer_class = OrganizerListSerializer
+
+
+class OrganizerDetailView(generics.RetrieveAPIView):
+    """ Organizer Detail View """
+    queryset = Organizer.objects.all()
+    lookup_field = 'profile_url'
+    serializer_class = OrganizerDetailSerializer
