4b0f50f34c58c90b19a0ba6ddc6d65ddee2a204a
Andrei Baliyevich <andreibaliyevich@gmail.com>
2022-02-23 11:25:08 +0300

    Add Organizer Filter

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import OrganizerFilter
 from .models import Organizer
 from .serializers import OrganizerListSerializer, OrganizerDetailSerializer
 
@@ -7,6 +9,8 @@ class OrganizerListView(generics.ListAPIView):
     """ Organizer List View """
     queryset = Organizer.objects.all()
     serializer_class = OrganizerListSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = OrganizerFilter
 
 
 class OrganizerDetailView(generics.RetrieveAPIView):
