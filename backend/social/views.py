bc3bad85317ea018185ac6bfa47bc72a07b4da0a
Andrei Baliyevich <andreibaliyevich@gmail.com>
2022-03-13 14:43:35 +0300

    Update Comment Create View

--- backend/social/views.py
+++ backend/social/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.db import IntegrityError
 from blog.models import Article
@@ -19,37 +20,35 @@ class NotificationListView(generics.ListAPIView):
 class CommentCreateView(generics.CreateAPIView):
     """ Comment Create View """
     serializer_class = CommentCreateSerializer
+    permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            serializer = self.get_serializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
 
-            if serializer.is_valid():
-                if kwargs['obj_type'] == 'article':
-                    content_obj = Article.objects.get(id=kwargs['obj_id'])
-                elif kwargs['obj_type'] == 'album':
-                    content_obj = Album.objects.get(id=kwargs['obj_id'])
-                elif kwargs['obj_type'] == 'photo':
-                    content_obj = Photo.objects.get(id=kwargs['obj_id'])
-                elif kwargs['obj_type'] == 'comment':
-                    content_obj = Comment.objects.get(id=kwargs['obj_id'])
-                else:
-                    return Response(status=status.HTTP_400_BAD_REQUEST)
-
-                try:
-                    serializer.save(
-                        user=request.user,
-                        content_object=content_obj,
-                    )
-                except IntegrityError:
-                    return Response(status=status.HTTP_409_CONFLICT)
-
-                headers = self.get_success_headers(serializer.data)
-                return Response(
-                    serializer.data,
-                    status=status.HTTP_201_CREATED,
-                    headers=headers)
+        if serializer.is_valid():
+            if kwargs['obj_type'] == 'article':
+                content_obj = Article.objects.get(id=kwargs['obj_id'])
+            elif kwargs['obj_type'] == 'album':
+                content_obj = Album.objects.get(id=kwargs['obj_id'])
+            elif kwargs['obj_type'] == 'photo':
+                content_obj = Photo.objects.get(id=kwargs['obj_id'])
+            elif kwargs['obj_type'] == 'comment':
+                content_obj = Comment.objects.get(id=kwargs['obj_id'])
             else:
                 return Response(status=status.HTTP_400_BAD_REQUEST)
+
+            try:
+                serializer.save(
+                    user=request.user,
+                    content_object=content_obj,
+                )
+            except IntegrityError:
+                return Response(status=status.HTTP_409_CONFLICT)
+
+            headers = self.get_success_headers(serializer.data)
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED,
+                headers=headers)
         else:
-            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
