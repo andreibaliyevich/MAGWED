b6036cf39273a9aeeda67c131dfb8f8e6074408b
Andrei Baliyevich <andreibaliyevich@gmail.com>
2022-03-31 19:36:58 +0300

    Update frontend settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,5 +10,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true
+    }
   }
 })
