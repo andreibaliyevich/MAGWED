3f5d0f6747466c4a01b2b6745ba3946a4dcea862
Andrei Baliyevich <andreibaliyevich@gmail.com>
2022-04-02 12:42:37 +0300

    Update styles and templates

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>This is a hone page</h1>
   </main>
 </template>
