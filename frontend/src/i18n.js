3fa321da90075fb40a0054ec8515cd8bd93b8ea5
Andrei Baliyevich <andreibaliyevich@gmail.com>
2022-04-06 11:59:32 +0300

    Update i18n.js

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,14 +1,34 @@
 import { createI18n } from 'vue-i18n'
-import en from '@/locales/en.json'
-import ru from '@/locales/ru.json'
-import be from '@/locales/be.json'
-import uk from '@/locales/uk.json'
+import en from './locales/en.json'
+import ru from './locales/ru.json'
+import be from './locales/be.json'
+import uk from './locales/uk.json'
 
+const availableLanguages = ['en', 'ru', 'be', 'uk'];
 
-export default createI18n({
+function getLanguage() {
+  const navigatorLanguage =
+    navigator.languages !== undefined
+      ? navigator.languages[0]
+      : navigator.language;
+
+  if (!navigatorLanguage) {
+    return availableLanguages[0];
+  }
+
+  const trimmedLanguage = navigatorLanguage.trim().split(/-|_/)[0];
+
+  if (!availableLanguages.includes(trimmedLanguage)) {
+    return availableLanguages[0];
+  }
+
+  return trimmedLanguage;
+}
+
+const i18n = createI18n({
   legacy: false,
   globalInjection: true,
-  locale: 'en',
+  locale: getLanguage(),
   fallbackLocale: 'en',
   messages: {
     en: en,
@@ -17,3 +37,5 @@ export default createI18n({
     uk: uk
   }
 })
+
+export default i18n
