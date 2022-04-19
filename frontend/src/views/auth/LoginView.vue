28a63f9c156d43c571a6d5c153a42601e3c058db
Andrei Baliyevich <andreibaliyevich@gmail.com>
2022-04-19 19:29:45 +0300

    Update LoginView.vue

--- frontend/src/views/auth/LoginView.vue
+++ frontend/src/views/auth/LoginView.vue
@@ -32,7 +32,7 @@ export default {
         }
       })
       .catch((error) => {
-        this.error = error
+        this.error = error.response.data
       })
     }
   }
@@ -44,7 +44,7 @@ export default {
     <h4>{{ $t('auth.login') }}</h4>
     <div class="input-group">
       <form @submit.prevent="login">
-        <input v-model="username" type="text" name="username" :placeholder="$t('auth.username')" class="form-control">
+        <input v-model="username" type="text" name="username" :placeholder="$t('auth.username_email')" class="form-control">
         <input v-model="password" type="password" name="password" :placeholder="$t('auth.password')" class="form-control">
         <button type="submit" class="btn btn-primary">{{ $t('auth.login') }}</button>
       </form>
