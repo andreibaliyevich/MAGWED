213fc204f9e63aba519b621a3b60448a9257ec48
Andrei Baliyevich <andreibaliyevich@gmail.com>
2022-05-22 00:53:14 +0300

    Update NavBar

--- frontend/src/components/base/NavBar.vue
+++ frontend/src/components/base/NavBar.vue
@@ -1,5 +1,11 @@
 <script setup>
 import { RouterLink } from 'vue-router'
+import axios from 'axios'
+
+import { useBaseStore } from '@/stores/base.js'
+import { useUserStore } from '@/stores/user.js'
+const baseStore = useBaseStore()
+const userStore = useUserStore()
 </script>
 
 <script>
@@ -7,6 +13,13 @@ export default {
   methods: {
     hideOffcanvas() {
       document.getElementById("offcanvasClose").click()
+    },
+    logout() {
+      axios.post('/en/accounts/auth/logout/')
+      .then(() => {
+        window.localStorage.removeItem('user')
+        window.location.reload()
+      })
     }
   }
 }
@@ -14,52 +27,96 @@ export default {
 
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm sticky-top">
-    <div class="container py-1 py-lg-3">
-      <RouterLink class="navbar-brand" :to="{ name: 'Home', params: { locale: `${ $i18n.locale }` }}">
-        <img width="190" src="/logo-navbar.png">
-      </RouterLink>
-      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+    <div class="container py-lg-2">
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">
         <span class="navbar-toggler-icon"></span>
       </button>
-
-      <div class="w-100 d-flex d-none d-lg-inline-flex mx-3">
-        <form class="input-group border rounded-pill d-flex align-items-center">
-          <label class="text-secondary ms-2" for="search-lg"><i class="fa-solid fa-magnifying-glass"></i></label>
-          <input class="form-control rounded-pill border-0 shadow-none m-1" id="search-lg" type="search" placeholder="Search..." aria-label="Search">
-        </form>
-      </div>
-
-      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <RouterLink class="navbar-brand d-flex d-sm-none align-items-center me-0" :to="{ name: 'Home', params: { locale: `${ $i18n.locale }` }}">
+        <img width="53" src="/logo-navbar.png">
+      </RouterLink>
+      <RouterLink class="navbar-brand d-flex d-none d-sm-inline-flex align-items-center me-0" :to="{ name: 'Home', params: { locale: `${ $i18n.locale }` }}">
+        <img width="190" src="/logo-navbar-full.png">
+      </RouterLink>
+      <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasMenu" aria-labelledby="offcanvasNavbarLabel">
         <div class="offcanvas-header">
           <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
           <button id="offcanvasClose" type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
-          <div class="d-flex d-lg-none">
-            <form class="input-group border rounded-pill d-flex align-items-center">
-              <label class="text-secondary ms-2" for="search-sm"><i class="fa-solid fa-magnifying-glass"></i></label>
-              <input class="form-control rounded-pill border-0 shadow-none m-1" id="search-sm" type="search" placeholder="Search..." aria-label="Search">
-            </form>
-          </div>
-          <ul class="navbar-nav justify-content-end flex-grow-1 ms-1">
+          <ul class="navbar-nav d-flex align-items-lg-center flex-grow-1 mx-3">
             <li class="nav-item text-uppercase fw-bold">
               <RouterLink v-if="this.$route.name == 'Home'" class="nav-link active" aria-current="page" :to="{ name: 'Home', params: { locale: `${ $i18n.locale }` }}" @click="hideOffcanvas">{{ $t('nav.best') }}</RouterLink>
               <RouterLink v-else class="nav-link text-dark" :to="{ name: 'Home', params: { locale: `${ $i18n.locale }` }}" @click="hideOffcanvas">{{ $t('nav.best') }}</RouterLink>
             </li>
-            <li class="nav-item text-uppercase fw-bold ms-lg-3">
+            <li class="nav-item text-uppercase fw-bold ms-xl-3">
               <RouterLink v-if="this.$route.name == 'Home'" class="nav-link active" aria-current="page" :to="{ name: 'Home', params: { locale: `${ $i18n.locale }` }}" @click="hideOffcanvas">{{ $t('nav.places') }}</RouterLink>
               <RouterLink v-else class="nav-link text-dark" :to="{ name: 'Home', params: { locale: `${ $i18n.locale }` }}" @click="hideOffcanvas">{{ $t('nav.places') }}</RouterLink>
             </li>
-            <li class="nav-item text-uppercase fw-bold ms-lg-3">
+            <li class="nav-item text-uppercase fw-bold ms-xl-3">
               <RouterLink v-if="this.$route.name == 'Home'" class="nav-link active" aria-current="page" :to="{ name: 'Home', params: { locale: `${ $i18n.locale }` }}" @click="hideOffcanvas">{{ $t('nav.awards') }}</RouterLink>
               <RouterLink v-else class="nav-link text-dark" :to="{ name: 'Home', params: { locale: `${ $i18n.locale }` }}" @click="hideOffcanvas">{{ $t('nav.awards') }}</RouterLink>
             </li>
-            <li class="nav-item text-uppercase fw-bold ms-lg-3">
+            <li class="nav-item text-uppercase fw-bold ms-xl-3">
               <RouterLink v-if="this.$route.name == 'Blog'" class="nav-link active" aria-current="page" :to="{ name: 'Blog', params: { locale: `${ $i18n.locale }` }}" @click="hideOffcanvas">{{ $t('nav.blog') }}</RouterLink>
               <RouterLink v-else class="nav-link text-dark" :to="{ name: 'Blog', params: { locale: `${ $i18n.locale }` }}" @click="hideOffcanvas">{{ $t('nav.blog') }}</RouterLink>
             </li>
           </ul>
+          <form class="hf-within border rounded-pill d-flex align-items-center mt-3 mt-lg-0">
+            <label class="text-secondary ms-2" for="search"><i class="fa-solid fa-magnifying-glass"></i></label>
+            <input class="form-control rounded-pill border-0 shadow-none m-1" id="search" type="search" placeholder="Search..." aria-label="Search">
+          </form>
         </div>
+      </div>
+      <div v-if="userStore.isLoggedIn" class="d-flex align-items-center ms-lg-3">
+        <div class="dropdown">
+          <a href="#" class="dropdown-toggle d-flex align-items-center text-decoration-none text-decoration-none text-dark" id="dropdownUser" data-bs-toggle="dropdown" aria-expanded="false">
+            <img v-if="userStore.avatar" :src="`${ baseStore.apiURL }${ userStore.avatar }`" class="rounded-circle" width="32" height="32" alt="avatar">
+            <img v-else src="/avatar.jpg" class="rounded-circle" width="32" height="32" alt="avatar">
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownUser">
+            <li>
+              <RouterLink v-if="this.$route.name == 'Notifications'" class="dropdown-item d-flex gap-2 align-items-center active" :to="{ name: 'Notifications', params: { locale: `${ $i18n.locale }` }}">
+                <i class="fa-solid fa-bell"></i>
+                {{ $t('auth.notifications') }}
+              </RouterLink>
+              <RouterLink v-else class="dropdown-item d-flex gap-2 align-items-center" :to="{ name: 'Notifications', params: { locale: `${ $i18n.locale }` }}">
+                <i class="fa-solid fa-bell"></i>
+                {{ $t('auth.notifications') }}
+              </RouterLink>
+            </li>
+            <li>
+              <RouterLink v-if="this.$route.name == 'Messenger'" class="dropdown-item d-flex gap-2 align-items-center active" :to="{ name: 'Messenger', params: { locale: `${ $i18n.locale }` }}">
+                <i class="fa-solid fa-comments"></i>
+                {{ $t('auth.messenger') }}
+              </RouterLink>
+              <RouterLink v-else class="dropdown-item d-flex gap-2 align-items-center" :to="{ name: 'Messenger', params: { locale: `${ $i18n.locale }` }}">
+                <i class="fa-solid fa-comments"></i>
+                {{ $t('auth.messenger') }}
+              </RouterLink>
+            </li>
+            <li>
+              <RouterLink v-if="this.$route.name == 'Profile'" class="dropdown-item d-flex gap-2 align-items-center active" :to="{ name: 'Profile', params: { locale: `${ $i18n.locale }` }}">
+                <i class="fa-solid fa-user"></i>
+                {{ $t('auth.profile.profile') }}
+              </RouterLink>
+              <RouterLink v-else class="dropdown-item d-flex gap-2 align-items-center" :to="{ name: 'Profile', params: { locale: `${ $i18n.locale }` }}">
+                <i class="fa-solid fa-user"></i>
+                {{ $t('auth.profile.profile') }}
+              </RouterLink>
+            </li>
+            <li><hr class="dropdown-divider"></li>
+            <li>
+              <div class="dropdown-item d-flex gap-2 align-items-center" @click="logout">
+                <i class="fa-solid fa-right-from-bracket"></i>
+                {{ $t('auth.logout') }}
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div v-else class="d-flex align-items-center ms-lg-3">
+        <RouterLink class="btn btn-outline-brand border-secondary rounded-pill px-3" :to="{ name: 'Login', params: { locale: `${ $i18n.locale }` }}" @click="hideOffcanvas">{{ $t('auth.login') }}</RouterLink>
+        <RouterLink class="btn btn-brand rounded-pill px-3 ms-1 d-flex d-none d-md-inline-flex" :to="{ name: 'Registration', params: { locale: `${ $i18n.locale }` }}" @click="hideOffcanvas">{{ $t('auth.registration') }}</RouterLink>
       </div>
     </div>
   </nav>
