6c3560a0b38b5e90225531315249a4046e86773d
Andrei Baliyevich <andreibaliyevich@gmail.com>
2022-09-03 10:36:12 +0300

    Add LocaleRouterLink.vue

---
+++ frontend/src/components/UI/LocaleRouterLink.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<script>
+export default {
+  name: 'LocaleRouterLink',
+  props: {
+    toName: {
+      type: String,
+      required: true
+    },
+    toParams: {
+      type: Object,
+      required: false
+    }
+  }
+}
+</script>
+
+<template>
+  <RouterLink
+    :to="{
+      name: `${ toName }`,
+      params: {
+        locale: `${ $i18n.locale }`,
+        ...toParams
+      }
+    }"
+  >
+    <slot></slot>
+  </RouterLink>
+</template>
