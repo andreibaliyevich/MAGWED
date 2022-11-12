7082f189ee07354650cb548a51fd8e2f5476a31d
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2022-11-12 13:36:18 +0300

    Update BaseInput component (frontend)

--- frontend/src/components/UI/BaseInput.vue
+++ frontend/src/components/UI/BaseInput.vue
@@ -24,37 +24,14 @@ export default {
 </script>
 
 <template>
-  <div
-    v-if="errors"
-    class="form-floating"
-  >
+  <div class="form-floating">
     <input
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
       :id="id"
       :placeholder="label"
       v-bind="$attrs"
-      class="form-control is-invalid"
-    >
-    <label v-if="label" :for="id">{{ label }}</label>
-    <div
-      v-for="error in errors"
-      class="invalid-feedback"
-    >
-      {{ error }}
-    </div>
-  </div>
-  <div
-    v-else
-    class="form-floating"
-  >
-    <input
-      :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
-      :id="id"
-      :placeholder="label"
-      v-bind="$attrs"
-      class="form-control"
+      :class="['form-control', { 'is-invalid': errors }]"
     >
     <label
       v-if="label"
@@ -62,5 +39,12 @@ export default {
     >
       {{ label }}
     </label>
+    <div
+      v-if="errors"
+      v-for="error in errors"
+      class="invalid-feedback"
+    >
+      {{ error }}
+    </div>
   </div>
 </template>
