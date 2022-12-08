bc0f0a50b7a48869aa10d5d552821c3eca559545
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2022-12-09 00:11:28 +0300

    Update RegistrationView.vue

--- frontend/src/views/auth/RegistrationView.vue
+++ frontend/src/views/auth/RegistrationView.vue
@@ -12,12 +12,19 @@ export default {
       password2: '',
       userType: '',
       name: '',
+      userTypeOptions: [],
       actionProcessing: false,
       status: null,
       errors: null
     }
   },
   methods: {
+    setUserTypeOptions() {
+      this.userTypeOptions = [
+        { value: 2, text: this.$t('auth.registration.customer') },
+        { value: 3, text: this.$t('auth.registration.organizer') }
+      ]
+    },
     registration() {
       this.actionProcessing = true
       axios.post('/accounts/auth/registration/', {
@@ -44,6 +51,14 @@ export default {
         document.documentElement.scrollTop = 0
       })
     }
+  },
+  watch: {
+    '$i18n.locale'(newValue) {
+      this.setUserTypeOptions()
+    },
+  },
+  mounted() {
+    this.setUserTypeOptions()
   }
 }
 </script>
@@ -82,246 +97,123 @@ export default {
         </LocaleRouterLink>
       </p>
 
-      <div
-        v-if="errors && errors.username"
-        class="form-floating mb-3"
-      >
-        <input
-          v-model="username"
-          id="id_username"
-          name="username"
-          type="text"
-          required=""
-          :placeholder="$t('auth.registration.username')"
-          class="form-control is-invalid"
-        >
-        <label for="id_username">{{ $t('auth.registration.username') }}</label>
-        <div
-          v-for="error in errors.username"
-          class="invalid-feedback"
-        >
-          {{ error }}
+      <div class="row g-3 mb-3">
+        <div class="col-md-12">
+          <BaseInput
+            v-if="errors && errors.username"
+            v-model="username"
+            :label="$t('auth.registration.username')"
+            :errors="errors.username"
+            id="id_username"
+            name="username"
+            type="text"
+          />
+          <BaseInput
+            v-else
+            v-model="username"
+            :label="$t('auth.registration.username')"
+            id="id_username"
+            name="username"
+            type="text"
+          />
         </div>
-      </div>
-      <div
-        v-else
-        class="form-floating mb-3"
-      >
-        <input
-          v-model="username"
-          id="id_username"
-          name="username"
-          type="text"
-          required=""
-          :placeholder="$t('auth.registration.username')"
-          class="form-control"
-        >
-        <label for="id_username">{{ $t('auth.registration.username') }}</label>
-      </div>
-
-      <div
-        v-if="errors && errors.email"
-        class="form-floating mb-3"
-      >
-        <input
-          v-model="email"
-          id="id_email"
-          name="email"
-          type="email"
-          maxlength="254"
-          required=""
-          :placeholder="$t('auth.registration.email')"
-          class="form-control is-invalid"
-        >
-        <label for="id_email">{{ $t('auth.registration.email') }}</label>
-        <div
-          v-for="error in errors.email"
-          class="invalid-feedback"
-        >
-          {{ error }}
+        <div class="col-md-12">
+          <BaseInput
+            v-if="errors && errors.email"
+            v-model="email"
+            :label="$t('auth.registration.email')"
+            :errors="errors.email"
+            id="id_email"
+            name="email"
+            type="email"
+            maxlength="254"
+          />
+          <BaseInput
+            v-else
+            v-model="email"
+            :label="$t('auth.registration.email')"
+            id="id_email"
+            name="email"
+            type="email"
+            maxlength="254"
+          />
         </div>
-      </div>
-      <div
-        v-else
-        class="form-floating mb-3"
-      >
-        <input
-          v-model="email"
-          id="id_email"
-          name="email"
-          type="email"
-          maxlength="254"
-          required=""
-          :placeholder="$t('auth.registration.email')"
-          class="form-control"
-        >
-        <label for="id_email">{{ $t('auth.registration.email') }}</label>
-      </div>
-
-      <div
-        v-if="errors && errors.password"
-        class="form-floating mb-3"
-      >
-        <input
-          v-model="password"
-          id="id_password"
-          name="password"
-          type="password"
-          required=""
-          :placeholder="$t('auth.password.password')"
-          class="form-control is-invalid"
-        >
-        <label for="id_password">{{ $t('auth.password.password') }}</label>
-        <div
-          v-for="error in errors.password"
-          class="invalid-feedback"
-        >
-          {{ error }}
+        <div class="col-md-12">
+          <BaseInput
+            v-if="errors && errors.password"
+            v-model="password"
+            :label="$t('auth.password.password')"
+            :errors="errors.password"
+            id="id_password"
+            name="password"
+            type="password"
+          />
+          <BaseInput
+            v-else
+            v-model="password"
+            :label="$t('auth.password.password')"
+            id="id_password"
+            name="password"
+            type="password"
+          />
         </div>
-      </div>
-      <div
-        v-else
-        class="form-floating mb-3"
-      >
-        <input
-          v-model="password"
-          id="id_password"
-          name="password"
-          type="password"
-          required=""
-          :placeholder="$t('auth.password.password')"
-          class="form-control"
-        >
-        <label for="id_password">{{ $t('auth.password.password') }}</label>
-      </div>
-
-      <div
-        v-if="errors && errors.password2"
-        class="form-floating mb-3"
-      >
-        <input
-          v-model="password2"
-          id="id_password2"
-          name="password2"
-          type="password"
-          required=""
-          :placeholder="$t('auth.registration.password2')"
-          class="form-control is-invalid"
-        >
-        <label for="id_password">{{ $t('auth.registration.password2') }}</label>
-        <div
-          v-for="error in errors.password2"
-          class="invalid-feedback"
-        >
-          {{ error }}
+        <div class="col-md-12">
+          <BaseInput
+            v-if="errors && errors.password2"
+            v-model="password2"
+            :label="$t('auth.registration.password2')"
+            :errors="errors.password2"
+            id="id_password2"
+            name="password2"
+            type="password"
+          />
+          <BaseInput
+            v-else
+            v-model="password2"
+            :label="$t('auth.registration.password2')"
+            id="id_password2"
+            name="password2"
+            type="password"
+          />
         </div>
-      </div>
-      <div
-        v-else
-        class="form-floating mb-3"
-      >
-        <input
-          v-model="password2"
-          id="id_password2"
-          name="password2"
-          type="password"
-          required=""
-          :placeholder="$t('auth.registration.password2')"
-          class="form-control"
-        >
-        <label for="id_password">{{ $t('auth.registration.password2') }}</label>
-      </div>
-
-      <div
-        v-if="errors && errors.user_type"
-        class="form-floating mb-3"
-      >
-        <select
-          v-model="userType"
-          id="id_user_type"
-          name="user_type"
-          required=""
-          class="form-select is-invalid"
-        >
-          <option
-            disabled
-            value=""
-          >
-            {{ $t('auth.registration.please_select_user_type') }}
-          </option>
-          <option value="2">{{ $t('auth.registration.customer') }}</option>
-          <option value="3">{{ $t('auth.registration.organizer') }}</option>
-        </select>
-        <label for="id_user_type">
-          {{ $t('auth.registration.user_type') }}
-        </label>
-        <div
-          v-for="error in errors.user_type"
-          class="invalid-feedback"
-        >
-          {{ error }}
+        <div class="col-md-12">
+          <BaseSelect
+            v-if="errors && errors.user_type"
+            v-model="userType"
+            :label="$t('auth.registration.user_type')"
+            :options="userTypeOptions"
+            :errors="errors.user_type"
+            id="id_user_type"
+            name="user_type"
+          />
+          <BaseSelect
+            v-else
+            v-model="userType"
+            :label="$t('auth.registration.user_type')"
+            :options="userTypeOptions"
+            id="id_user_type"
+            name="user_type"
+          />
         </div>
-      </div>
-      <div
-        v-else
-        class="form-floating mb-3"
-      >
-        <select
-          v-model="userType"
-          id="id_user_type"
-          name="user_type"
-          required=""
-          class="form-select"
-        >
-          <option
-            disabled
-            value=""
-          >
-            {{ $t('auth.registration.please_select_user_type') }}
-          </option>
-          <option value="2">{{ $t('auth.registration.customer') }}</option>
-          <option value="3">{{ $t('auth.registration.organizer') }}</option>
-        </select>
-        <label for="id_user_type">
-          {{ $t('auth.registration.user_type') }}
-        </label>
-      </div>
-
-      <div
-        v-if="errors && errors.name"
-        class="form-floating mb-3"
-      >
-        <input
-          v-model="name"
-          id="id_name"
-          name="name"
-          type="text"
-          required=""
-          :placeholder="$t('auth.registration.name')"
-          class="form-control is-invalid"
-        >
-        <label for="id_username">{{ $t('auth.registration.name') }}</label>
-        <div
-          v-for="error in errors.name"
-          class="invalid-feedback"
-        >
-          {{ error }}
+        <div class="col-md-12">
+          <BaseInput
+            v-if="errors && errors.name"
+            v-model="name"
+            :label="$t('auth.registration.name')"
+            :errors="errors.name"
+            id="id_name"
+            name="name"
+            type="text"
+          />
+          <BaseInput
+            v-else
+            v-model="name"
+            :label="$t('auth.registration.name')"
+            id="id_name"
+            name="name"
+            type="text"
+          />
         </div>
-      </div>
-      <div
-        v-else
-        class="form-floating mb-3"
-      >
-        <input
-          v-model="name"
-          id="id_name"
-          name="name"
-          type="text"
-          required=""
-          :placeholder="$t('auth.registration.name')"
-          class="form-control"
-        >
-        <label for="id_username">{{ $t('auth.registration.name') }}</label>
       </div>
 
       <button
@@ -330,6 +222,7 @@ export default {
       >
         {{ $t('auth.signup') }}
       </button>
+
       <hr class="my-4">
       <div class="fs-6 text-muted">
         {{ $t('auth.registration.help') }}
