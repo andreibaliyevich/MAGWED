d098be5607c557de5826b9cca3f8642efe3cecff
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2023-01-16 22:57:37 +0300

    Decomposition main NavBar (frontend)

--- frontend/src/components/main/NavBar.vue
+++ frontend/src/components/main/NavBar.vue
@@ -1,9 +1,8 @@
 <script setup>
-import { API_URL, userType } from '@/config.js'
-import { useLogout } from '@/composables/logout.js'
 import { useUserStore } from '@/stores/user.js'
+import NavBarNotifications from './NavBarNotifications.vue'
+import NavBarUser from './NavBarUser.vue'
 
-const { logout } = useLogout()
 const userStore = useUserStore()
 </script>
 
@@ -187,147 +186,8 @@ const userStore = useUserStore()
             </span>
           </LocaleRouterLink>
         </div>
-        <div class="dropdown ms-3">
-          <a
-            class="link-secondary position-relative"
-            href="#"
-            role="button"
-            id="dropdownNotifications"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <i class="fa-solid fa-bell fa-lg"></i>
-            <span class="position-absolute top-0 start-100 translate-middle p-1 bg-danger rounded-circle">
-              <span class="visually-hidden">New notifications</span>
-            </span>
-          </a>
-          <ul
-            class="dropdown-menu dropdown-menu-end shadow w-100 overflow-auto"
-            aria-labelledby="dropdownNotifications"
-          >
-            <li><a class="dropdown-item" href="#">Notifications 1</a></li>
-            <li><a class="dropdown-item" href="#">Notifications 2</a></li>
-            <li><a class="dropdown-item" href="#">Notifications 3</a></li>
-            <li><a class="dropdown-item" href="#">Notifications 4</a></li>
-            <li><a class="dropdown-item" href="#">Notifications 5 (Extended, Extended, Extended)</a></li>
-            <li><a class="dropdown-item" href="#">Notifications 6</a></li>
-            <li><a class="dropdown-item" href="#">Notifications 7</a></li>
-            <li><a class="dropdown-item" href="#">Notifications 8</a></li>
-          </ul>
-        </div>
-        <div class="dropdown ms-3">
-          <a
-            ref="dropdownUser"
-            id="dropdownUser"
-            href="#"
-            class="d-flex align-items-center text-decoration-none text-decoration-none text-dark"
-            data-bs-toggle="dropdown"
-            data-bs-auto-close="outside"
-            aria-expanded="false"
-          >
-            <img
-              v-if="userStore.avatar"
-              :src="`${ API_URL }${ userStore.avatar }`"
-              class="rounded-circle"
-              width="32"
-              height="32"
-              alt="avatar"
-            >
-            <img
-              v-else
-              src="/user-avatar.jpg"
-              class="rounded-circle"
-              width="32"
-              height="32"
-              alt="avatar"
-            >
-          </a>
-          <ul
-            class="dropdown-menu dropdown-menu-end shadow"
-            aria-labelledby="dropdownUser"
-          >
-            <li class="px-3 pt-2 pb-3">
-              <div class="d-flex gap-2 align-items-center mb-1">
-                <img
-                  v-if="userStore.avatar"
-                  :src="`${ API_URL }${ userStore.avatar }`"
-                  class="rounded-circle"
-                  width="48"
-                  height="48"
-                  alt="avatar"
-                >
-                <img
-                  v-else
-                  src="/user-avatar.jpg"
-                  class="rounded-circle"
-                  width="48"
-                  height="48"
-                  alt="avatar"
-                >
-                <div class="ms-1">
-                  <span class="d-flex flex-nowrap fw-bolder">
-                    {{ userStore.name }}
-                  </span>
-                  <small class="text-muted">
-                    {{ userStore.email }}
-                  </small>
-                </div>
-              </div>
-              <LocaleRouterLink
-                routeName="Profile"
-                @click="$refs.dropdownUser.click()"
-                class="w-100 btn btn-light-brand btn-sm text-center"
-              >
-                {{ $t('auth.profile.edit_profile') }}
-              </LocaleRouterLink>
-            </li>
-            <li>
-              <LocaleRouterLink
-                v-if="userStore.userType == userType.ORGANIZER"
-                routeName="Home"
-                @click="$refs.dropdownUser.click()"
-                class="dropdown-item d-flex gap-2 align-items-center"
-              >
-                <i class="fa-solid fa-users"></i>
-                {{ $t('auth.followers') }}
-              </LocaleRouterLink>
-              <LocaleRouterLink
-                routeName="Home"
-                @click="$refs.dropdownUser.click()"
-                class="dropdown-item d-flex gap-2 align-items-center"
-              >
-                <i class="fa-solid fa-user-group"></i>
-                {{ $t('auth.following') }}
-              </LocaleRouterLink>
-              <LocaleRouterLink
-                routeName="Home"
-                @click="$refs.dropdownUser.click()"
-                class="dropdown-item d-flex gap-2 align-items-center"
-              >
-                <i class="fa-solid fa-star"></i>
-                {{ $t('auth.favorites') }}
-              </LocaleRouterLink>
-              <LocaleRouterLink
-                routeName="Home"
-                @click="$refs.dropdownUser.click()"
-                class="dropdown-item d-flex gap-2 align-items-center"
-              >
-                <i class="fa-solid fa-comment-dots"></i>
-                {{ $t('footer.feedback') }}
-              </LocaleRouterLink>
-            </li>
-            <li><hr class="dropdown-divider"></li>
-            <li>
-              <div
-                @click="logout()"
-                class="dropdown-item d-flex gap-2 align-items-center"
-              >
-                <i class="fa-solid fa-right-from-bracket"></i>
-                {{ $t('auth.log_out') }}
-              </div>
-            </li>
-          </ul>
-        </div>
+        <NavBarNotifications />
+        <NavBarUser />
       </div>
       <div
         v-else
---
+++ frontend/src/components/main/NavBarNotifications.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="dropdown-notifications">
+    <div class="dropdown ms-3">
+      <a
+        class="link-secondary position-relative"
+        href="#"
+        role="button"
+        id="dropdownNotifications"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        <i class="fa-solid fa-bell fa-lg"></i>
+        <span class="position-absolute top-0 start-100 translate-middle p-1 bg-danger rounded-circle">
+          <span class="visually-hidden">New notifications</span>
+        </span>
+      </a>
+      <ul
+        class="dropdown-menu dropdown-menu-end shadow w-100 overflow-auto"
+        aria-labelledby="dropdownNotifications"
+      >
+        <li><a class="dropdown-item" href="#">Notifications 1</a></li>
+        <li><a class="dropdown-item" href="#">Notifications 2</a></li>
+        <li><a class="dropdown-item" href="#">Notifications 3</a></li>
+        <li><a class="dropdown-item" href="#">Notifications 4</a></li>
+        <li><a class="dropdown-item" href="#">Notifications 5 (Extended, Extended, Extended)</a></li>
+        <li><a class="dropdown-item" href="#">Notifications 6</a></li>
+        <li><a class="dropdown-item" href="#">Notifications 7</a></li>
+        <li><a class="dropdown-item" href="#">Notifications 8</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
