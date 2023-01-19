92fc101c2e47b8d34a19427051cb12311e02e4ef
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2023-01-19 09:29:33 +0300

    Add UserShortReadSerializer for NotificationListSerializer (backend)

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
+from accounts.serializers import UserShortReadSerializer
 from .models import Notification
 from .serializer_fields import NotificationObjectRelatedField
 
 class NotificationListSerializer(serializers.ModelSerializer):
     """ Notification List Serializer """
+    initiator = UserShortReadSerializer(read_only=True)
     content_object = NotificationObjectRelatedField(read_only=True)
 
     class Meta:
