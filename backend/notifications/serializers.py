b8afead51d12af2121ec5a4b5ff0d23141aa385e
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2023-01-19 10:26:45 +0300

    Update serializers of notifications app (backend)

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -1,7 +1,38 @@
 from rest_framework import serializers
 from accounts.serializers import UserShortReadSerializer
+from blog.models import Article
+from blog.serializers import ArticleShortReadSerializer
+from portfolio.models import Album, Photo
+from portfolio.serializers import (
+    AlbumShortReadSerializer,
+    PhotoShortReadSerializer,
+)
+from social.models import Follow, Comment, Review
 from .models import Notification
-from .serializer_fields import NotificationObjectRelatedField
+
+
+class NotificationObjectRelatedField(serializers.RelatedField):
+    """
+    A custom field to use for the 'content_object' generic relationship.
+    """
+
+    def to_representation(self, value):
+        if isinstance(value, Follow):
+            return None
+        elif isinstance(value, Article):
+            serializer = ArticleShortReadSerializer(value)
+        elif isinstance(value, Album):
+            serializer = AlbumShortReadSerializer(value)
+        elif isinstance(value, Photo):
+            serializer = PhotoShortReadSerializer(value)
+        elif isinstance(value, Comment):
+            return None
+        elif isinstance(value, Review):
+            return None
+        else:
+            raise Exception('Unexpected type of content object')
+        return serializer.data
+
 
 class NotificationListSerializer(serializers.ModelSerializer):
     """ Notification List Serializer """
