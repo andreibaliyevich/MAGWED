eb1caee4e25baec0751b95494f847bfdf93f5ced
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2023-01-28 19:07:45 +0300

    Set VueI18nPlugin (frontend)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,17 @@
 import { fileURLToPath, URL } from 'url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VueI18nPlugin()
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue-i18n': 'vue-i18n/index'
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   server: {
