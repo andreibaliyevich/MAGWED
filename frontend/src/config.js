030a85df635778360e52b7301ccd8b1096abe5e7
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2023-01-28 19:17:49 +0300

    Update config (frontend)

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -17,3 +17,17 @@ export const messageType = {
   IMAGES: 2,
   FILES: 3
 }
+
+export const reasonOfNotification = {
+  FOLLOW: 1,
+  ARTICLE: 2,
+  ALBUM: 3,
+  PHOTO: 4,
+  LIKE_ALBUM: 5,
+  LIKE_PHOTO: 6,
+  COMMENT_ARTICLE: 7,
+  COMMENT_ALBUM: 8,
+  COMMENT_PHOTO: 9,
+  COMMENT: 10,
+  REVIEW: 11
+}
