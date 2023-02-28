eae48b0072eaceaa1d9619910d98954af3022a4d
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2023-02-28 07:01:29 +0300

    Add ListInput (frontend)

--- frontend/src/components/UI/index.js
+++ frontend/src/components/UI/index.js
@@ -2,6 +2,7 @@ import BaseInput from './BaseInput.vue'
 import BaseSelect from './BaseSelect.vue'
 import BaseTextarea from './BaseTextarea.vue'
 import FileInputButton from './FileInputButton.vue'
+import ListInput from './ListInput.vue'
 import LoadingIndicator from './LoadingIndicator.vue'
 import LocaleRouterLink from './LocaleRouterLink.vue'
 import MultipleSelect from './MultipleSelect.vue'
@@ -13,6 +14,7 @@ export default [
   BaseSelect,
   BaseTextarea,
   FileInputButton,
+  ListInput,
   LoadingIndicator,
   LocaleRouterLink,
   MultipleSelect,
