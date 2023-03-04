899ba6b56cafa2a5d4cfdc91c07634328601f9ba
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2023-03-04 14:37:12 +0300

    Update logout (frontend)

--- frontend/src/composables/logout.js
+++ frontend/src/composables/logout.js
@@ -1,12 +1,22 @@
 import axios from 'axios'
+import { useRouter } from 'vue-router'
+import { useI18n } from 'vue-i18n'
+import { useUserStore } from '@/stores/user.js'
 
 export function useLogout() {
+  const router = useRouter()
+  const { locale } = useI18n({ useScope: 'global' })
+  const userStore = useUserStore()
 
   const logout = () => {
     axios.post('/accounts/auth/logout/')
     .then(() => {
       window.localStorage.removeItem('user')
-      window.location.reload()
+      userStore.$reset()
+      router.push({
+        name: 'Home',
+        params: { locale: locale.value }
+      })
     })
   }
 
