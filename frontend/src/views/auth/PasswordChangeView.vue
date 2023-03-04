8a716e3cf43ce0ed67ee31bf481014362d66aa51
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2023-03-04 15:22:00 +0300

    Update PasswordChangeView.vue (frontend)

--- frontend/src/views/auth/PasswordChangeView.vue
+++ frontend/src/views/auth/PasswordChangeView.vue
@@ -41,103 +41,99 @@ const changePassword = async () => {
 
 <template>
   <div class="password-change-view">
-    <h1 class="display-6 mb-5">
-      {{ $t('auth.passwordchange.password_change') }}
-    </h1>
+    <div class="px-1 px-lg-3 px-xl-5">
+      <h1 class="display-6 mb-5">
+        {{ $t('auth.passwordchange.password_change') }}
+      </h1>
 
-    <div
-      v-if="status"
-      class="px-md-5"
-    >
-      <div
-        class="alert alert-success d-flex align-items-center alert-dismissible fade show"
-        role="alert"
-      >
-        <i class="fa-solid fa-circle-check"></i>
-        <div class="ms-3">{{ status }}</div>
-        <button
-          @click="status = null"
-          type="button"
-          class="btn-close"
-          data-bs-dismiss="alert"
-          aria-label="Close"
-        ></button>
+      <div v-if="status">
+        <div
+          class="alert alert-success d-flex align-items-center alert-dismissible fade show"
+          role="alert"
+        >
+          <i class="fa-solid fa-circle-check"></i>
+          <div class="ms-3">{{ status }}</div>
+          <button
+            @click="status = null"
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+          ></button>
+        </div>
       </div>
+
+      <form @submit.prevent="changePassword()">
+        <div class="mb-3">
+          <BaseInput
+            v-if="errors && errors.current_password"
+            v-model="currentPassword"
+            :label="$t('auth.passwordchange.current_password')"
+            :errors="errors.current_password"
+            id="id_current_password"
+            name="current_password"
+            type="password"
+          />
+          <BaseInput
+            v-else
+            v-model="currentPassword"
+            :label="$t('auth.passwordchange.current_password')"
+            id="id_current_password"
+            name="current_password"
+            type="password"
+          />
+        </div>
+        <div class="mb-3">
+          <BaseInput
+            v-if="errors && errors.new_password"
+            v-model="newPassword"
+            :label="$t('auth.password.new_password')"
+            :errors="errors.new_password"
+            id="id_new_password"
+            name="new_password"
+            type="password"
+          />
+          <BaseInput
+            v-else
+            v-model="newPassword"
+            :label="$t('auth.password.new_password')"
+            id="id_new_password"
+            name="new_password"
+            type="password"
+          />
+        </div>
+        <ul class="fs-6">
+          <li>{{ $t('auth.password.advice1') }}</li>
+          <li>{{ $t('auth.password.advice2') }}</li>
+          <li>{{ $t('auth.password.advice3') }}</li>
+          <li>{{ $t('auth.password.advice4') }}</li>
+        </ul>
+        <div class="mb-3">
+          <BaseInput
+            v-if="errors && errors.new_password2"
+            v-model="newPassword2"
+            :label="$t('auth.password.new_password2')"
+            :errors="errors.new_password2"
+            id="id_new_password2"
+            name="new_password2"
+            type="password"
+          />
+          <BaseInput
+            v-else
+            v-model="newPassword2"
+            :label="$t('auth.password.new_password2')"
+            id="id_new_password2"
+            name="new_password2"
+            type="password"
+          />
+        </div>
+        <SubmitButton
+          :loadingStatus="passwordUpdating"
+          buttonClass="btn btn-brand btn-lg"
+        >
+          {{ $t('auth.password.change_password') }}
+        </SubmitButton>
+      </form>
     </div>
-
-    <form
-      @submit.prevent="changePassword()"
-      class="px-md-5"
-    >
-      <div class="mb-3">
-        <BaseInput
-          v-if="errors && errors.current_password"
-          v-model="currentPassword"
-          :label="$t('auth.passwordchange.current_password')"
-          :errors="errors.current_password"
-          id="id_current_password"
-          name="current_password"
-          type="password"
-        />
-        <BaseInput
-          v-else
-          v-model="currentPassword"
-          :label="$t('auth.passwordchange.current_password')"
-          id="id_current_password"
-          name="current_password"
-          type="password"
-        />
-      </div>
-      <div class="mb-3">
-        <BaseInput
-          v-if="errors && errors.new_password"
-          v-model="newPassword"
-          :label="$t('auth.password.new_password')"
-          :errors="errors.new_password"
-          id="id_new_password"
-          name="new_password"
-          type="password"
-        />
-        <BaseInput
-          v-else
-          v-model="newPassword"
-          :label="$t('auth.password.new_password')"
-          id="id_new_password"
-          name="new_password"
-          type="password"
-        />
-      </div>
-      <ul class="fs-6">
-        <li>{{ $t('auth.password.advice1') }}</li>
-        <li>{{ $t('auth.password.advice2') }}</li>
-        <li>{{ $t('auth.password.advice3') }}</li>
-        <li>{{ $t('auth.password.advice4') }}</li>
-      </ul>
-      <div class="mb-3">
-        <BaseInput
-          v-if="errors && errors.new_password2"
-          v-model="newPassword2"
-          :label="$t('auth.password.new_password2')"
-          :errors="errors.new_password2"
-          id="id_new_password2"
-          name="new_password2"
-          type="password"
-        />
-        <BaseInput
-          v-else
-          v-model="newPassword2"
-          :label="$t('auth.password.new_password2')"
-          id="id_new_password2"
-          name="new_password2"
-          type="password"
-        />
-      </div>
-      <SubmitButton
-        :loadingStatus="passwordUpdating"
-        buttonClass="btn btn-brand btn-lg"
-      >
-        {{ $t('auth.password.change_password') }}
-      </SubmitButton>
-    </form>
   </div>
 </template>
