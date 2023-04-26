72fa3be4f95cce672246df9953f2a090abfd350e
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2023-04-26 07:12:36 +0300

    Add NumberRangeInput UI component (frontend)

--- frontend/src/components/UI/index.js
+++ frontend/src/components/UI/index.js
@@ -7,6 +7,7 @@ import InputMultipleSelect from './InputMultipleSelect.vue'
 import ListInput from './ListInput.vue'
 import LoadingIndicator from './LoadingIndicator.vue'
 import LocaleRouterLink from './LocaleRouterLink.vue'
+import NumberRangeInput from './NumberRangeInput.vue'
 import SubmitButton from './SubmitButton.vue'
 import UserAvatar from './UserAvatar.vue'
 import UserAvatarExtended from './UserAvatarExtended.vue'
@@ -21,6 +22,7 @@ export default [
   ListInput,
   LoadingIndicator,
   LocaleRouterLink,
+  NumberRangeInput,
   SubmitButton,
   UserAvatar,
   UserAvatarExtended
