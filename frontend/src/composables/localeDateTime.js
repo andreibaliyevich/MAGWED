9abe7c06aec2f9b9c4b26aa124a7685ea1ac3428
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2023-05-15 07:39:49 +0300

    Update localeDateTime composable (frontend)

--- frontend/src/composables/localeDateTime.js
+++ frontend/src/composables/localeDateTime.js
@@ -3,6 +3,13 @@ import { useI18n } from 'vue-i18n'
 export function useLocaleDateTime() {
   const { locale } = useI18n({ useScope: 'global' })
 
+  const getLocaleDateString = (dateTimeString) => {
+    const dateTime = new Date(dateTimeString)
+    return dateTime.toLocaleDateString(locale.value, {
+      dateStyle: 'medium'
+    })
+  }
+
   const getLocaleDateTimeString = (dateTimeString) => {
     const dateTime = new Date(dateTimeString)
     return dateTime.toLocaleTimeString(locale.value, {
@@ -12,5 +19,8 @@ export function useLocaleDateTime() {
     })
   }
 
-  return { getLocaleDateTimeString }
+  return {
+    getLocaleDateString,
+    getLocaleDateTimeString
+  }
 }
