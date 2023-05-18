e7e26349d85a96b2ea65d8c847527fdc9ee630c9
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2023-05-18 06:35:56 +0300

    Update roleTypeOptions (frontend)

--- frontend/src/composables/optionsOfRoleTypes.js
+++ frontend/src/composables/optionsOfRoleTypes.js
@@ -7,17 +7,17 @@ export function useOptionsOfRoleTypes() {
 
   const setRoleTypeOptions = () => {
     roleTypeOptions.value = [
-      { value: 1, text: t('roles.photographer') },
-      { value: 2, text: t('roles.videographer') },
-      { value: 3, text: t('roles.leading') },
-      { value: 4, text: t('roles.musician') },
-      { value: 5, text: t('roles.dj') },
-      { value: 6, text: t('roles.agency') },
-      { value: 7, text: t('roles.salon') },
-      { value: 8, text: t('roles.confectionery') },
-      { value: 9, text: t('roles.decorator') },
-      { value: 10, text: t('roles.visagiste') },
-      { value: 11, text: t('roles.hairdresser') }
+      { value: 1, text: t('roles.1') },
+      { value: 2, text: t('roles.2') },
+      { value: 3, text: t('roles.3') },
+      { value: 4, text: t('roles.4') },
+      { value: 5, text: t('roles.5') },
+      { value: 6, text: t('roles.6') },
+      { value: 7, text: t('roles.7') },
+      { value: 8, text: t('roles.8') },
+      { value: 9, text: t('roles.9') },
+      { value: 10, text: t('roles.10') },
+      { value: 11, text: t('roles.11') }
     ]
   }
 
