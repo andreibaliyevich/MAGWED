8135395af46c51097be728fa9a2f3f2eaca5765c
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2023-06-14 11:11:00 +0300

    Update router (frontend)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -18,7 +18,7 @@ function isAuthenticated(to, from) {
 }
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/:locale',
@@ -176,11 +176,6 @@ const router = createRouter({
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
       return savedPosition
-    } else if (to.hash) {
-      return {
-        el: to.hash,
-        behavior: 'smooth',
-      }
     } else {
       return { top: 0 }
     }
