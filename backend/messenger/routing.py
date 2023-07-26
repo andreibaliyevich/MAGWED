6dfeaa0e7ef71d66e7853cc401adfeb9ecaf7de1
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2023-07-26 22:47:57 +0300

    Update websocket urlpatterns (backend)

--- backend/messenger/routing.py
+++ backend/messenger/routing.py
@@ -3,6 +3,8 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r'^ws/messenger/(?P<convo_uuid>[0-9a-f-]+)/$',
-        consumers.MessengerConsumer.as_asgi()),
+    re_path(r'^ws/messenger/'
+            r'(?P<convo_uuid>[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12})/$',
+        consumers.MessengerConsumer.as_asgi(),
+    ),
 ]
