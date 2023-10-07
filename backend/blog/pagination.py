ec828fb9ac4474ae2cca11dd38f3e68df9ebeabe
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2023-10-07 13:12:02 +0300

    Update Pagination (backend)

--- backend/blog/pagination.py
+++ backend/blog/pagination.py
@@ -1,8 +1,7 @@
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
 
 
-class ArticlePagination(LimitOffsetPagination):
-    default_limit = 10
-    limit_query_param = 'limit'
-    offset_query_param = 'offset'
-    max_limit = 100
+class ArticlePagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
