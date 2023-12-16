c46051cdfd04c2874a618d8f7a41295b8a8ccf0d
Andrei Baliyevich <andreibaliyevich@gmail.com>
2023-12-17 00:15:59 +0300

    Update asgi.py (backend)

--- backend/magwed/asgi.py
+++ backend/magwed/asgi.py
@@ -11,14 +11,15 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magwed.settings')
+django_asgi_app = get_asgi_application()
+
 from accounts.middleware import WebSocketAuthMiddleware
 import accounts.routing
 import comments.routing
 import messenger.routing
 import notifications.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magwed.settings')
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
