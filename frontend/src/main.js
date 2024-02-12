f26f9414427aa859a5858088bacf446a97291320
Andrei Baliyevich <andreibaliyevich@gmail.com>
2024-02-12 18:31:41 +0300

    Update main.js (frontend)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,25 +1,16 @@
-import { createPinia } from 'pinia'
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
-
-import uiComponents from './components/UI'
-import directives from './directives'
+import vuetify from './vuetify'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
-
-uiComponents.forEach(component => {
-  app.component(component.name, component)
-})
-
-directives.forEach(directive => {
-  app.directive(directive.name, directive)
-})
+app.use(vuetify)
 
 app.mount('#app')
