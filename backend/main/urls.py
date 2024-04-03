dfd57ab52eb9c9603efb72aba183b578f7f07b52
Andrei Baliyevich <andreibaliyevich@gmail.com>
2024-04-03 19:35:53 +0300

    Update main urls (backend)

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('currencies/<str:code>/', views.CurrencyRetrieveView.as_view()),
     path('cities/', views.CityListView.as_view()),
     path('magazine/', views.MagazineView.as_view()),
-    path('tag/<uuid:uuid>/', views.TagRetrieveView.as_view()),
+    path('tags/<uuid:uuid>/', views.TagRetrieveView.as_view()),
 ]
