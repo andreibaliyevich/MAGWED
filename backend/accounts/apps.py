808e9d5fc9e4b6f5cbdf2b0cbb5dd6bb8ec3368a
Andrei Baliyevich <andreibaliyevich@gmail.com>
2024-04-22 01:25:39 +0300

    Fixing crashes (backend)

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -6,3 +6,8 @@ class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
     verbose_name = _('Accounts')
+
+    def ready(self):
+        from .models import ConnectionHistory
+
+        ConnectionHistory.objects.update(online=False)
--- backend/messenger/apps.py
+++ backend/messenger/apps.py
@@ -6,3 +6,8 @@ class MessengerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'messenger'
     verbose_name = _('Messenger')
+
+    def ready(self):
+        from django.core.cache import caches
+
+        caches['connections'].clear()
